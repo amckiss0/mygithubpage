b06e1fa29f444522b2e3dc26349bd64542dac68c
Ashley mckissick jr <55027769+amckiss0@users.noreply.github.com>
2025-04-05 18:36:32 -0400

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,24 @@
+    .character {
+        width: 100px;
+        height: 200px;
+        position: relative; /* For positioning elements */
+    }
+
+    .head {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-color: #f0f0f0;
+        position: absolute;
+        top: 0;
+        left: 25%;
+    }
+
+    .body {
+        width: 50px;
+        height: 100px;
+        background-color: #e0e0e0;
+        position: absolute;
+        top: 50px;
+        left: 25%;
+    }
